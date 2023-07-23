f9fa99b0baee39fb0bbc917a9e1c82d796be3151
sevalenciagr <sevalenciagr@gmail.com>
2023-07-23 14:45:07 -0500

    Cambios estilos

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,85 +1,209 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './header.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./header.css";
 
 const Header = () => {
   return (
     <nav className="navbar fixed-top navbar-expand-lg">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          <img src="https://vasscompany.com/wp-content/uploads/2020/07/logo-vass-blanco.png" alt="Logo" width="71" height="24" />
-        </a>        
-        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <img
+            src="https://vasscompany.com/wp-content/uploads/2020/07/logo-vass-blanco.png"
+            alt="Logo"
+            width="71"
+            height="24"
+          />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="offcanvas offcanvas-end" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div
+          className="offcanvas offcanvas-end"
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+        >
           <div className="offcanvas-header">
-            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+              <a className="navbar-brand" href="#">
+                <img
+                  src="https://vasscompany.com/wp-content/uploads/2020/07/logo-vass-blanco.png"
+                  alt="Logo"
+                  width="71"
+                  height="24"
+                />
+              </a>{" "}
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
           </div>
           <div className="offcanvas-body text-uppercase fw-bold">
             <p className="p_languaje d-lg-none">En</p>
             <ul className="navbar-nav justify-content-end flex-grow-1 pe-3 sm-fs-1">
               <li className="nav-item d-lg-none">
-                <a className="nav-link" aria-current="page" href="#">Home</a>
+                <a className="nav-link" aria-current="page" href="#">
+                  Home
+                </a>
               </li>
               <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Casos de éxito
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
                   Mercados
                 </a>
                 <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
                   <li>
                     <hr className="dropdown-divider" />
                   </li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
                 </ul>
               </li>
               <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
                   Cómo lo hacemos
                 </a>
                 <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
                   <li>
                     <hr className="dropdown-divider" />
                   </li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
                 </ul>
               </li>
               <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
                   Somos vass
                 </a>
                 <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
                   <li>
                     <hr className="dropdown-divider" />
                   </li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
                 </ul>
               </li>
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#">Insights</a>
+                <a className="nav-link" aria-current="page" href="#">
+                  Insights
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#">Contacto</a>
+                <a className="nav-link" aria-current="page" href="#">
+                  Contacto
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#">Vass research</a>
+                <a className="nav-link" aria-current="page" href="#">
+                  Vass research
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#">talento</a>
+                <a className="nav-link" aria-current="page" href="#">
+                  talento
+                </a>
               </li>
             </ul>
-
           </div>
         </div>
       </div>
     </nav>
-
   );
 };
 
